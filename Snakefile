8eae3e4172ac9885b10d30942f72a126b047e728
jurriaanjanssen <jurriaanjanssen1996@outlook.com>
2026-03-13 16:23:47 +0000

    Working tumor-only variant calling, initialize CNA script

--- Snakefile
+++ Snakefile
@@ -6,27 +6,30 @@ import pandas as pd
 data_dir = config["all"]["data_dir"]
 output_dir = config["all"]["output_dir"]
 # Fetch sample wildcards
+Patients = pd.read_csv(config['all']['samplesheet'])['patient'].to_numpy()
 Samples = pd.read_csv(config['all']['samplesheet'])['sample'].to_numpy()
 #-------------------------------------------------------------------------------------------------------------------
 # 0.2 specify target rules
 rule all:
     input:
-        expand("results/sarek/{sample}.done", sample = Samples[0])
+        expand(output_dir + "sarek/{patient}/annotation/mutect2/{patient}_tumor1/{patient}_tumor1.mutect2.filtered_snpEff_VEP.ann.vcf.gz", patient = Patients)
 
 #+++++++++++++++++++++++++++++++++++++++++ 1 RUN SAREK VARIANT CALLING +++++++++++++++++++++++++++++++++++++++++++++
-# 1.1 Run Sarek
+# 1.1 Run Sarek tumor-only variant calling
 rule Sarek:
     input:
         config['all']['samplesheet']
     output:
-        "results/sarek/{sample}.done"
+        samplesheet = output_dir + 'sarek/{patient}/csv/samplesheet.csv',
+        bam = output_dir + "sarek/{patient}/preprocessing/mapped/{patient}_tumor1/{patient}_tumor1.sorted.bam",
+        vcf = output_dir + "sarek/{patient}/annotation/mutect2/{patient}_tumor1/{patient}_tumor1.mutect2.filtered_snpEff_VEP.ann.vcf.gz"
     threads: 2
     resources:
         mem_mb=100000
     conda:
         "envs/nextflow.yaml"
     log:
-        "logs/sarek/{sample}/nextflow_"+datetime.now().strftime("%Y_%m_%d_%H%M%S")+".log"
+        "logs/sarek/{patient}/nextflow_"+datetime.now().strftime("%Y_%m_%d_%H%M%S")+".log"
     params:
         genome = 'GATK.GRCh38',
         profile = "singularity",
@@ -34,36 +37,55 @@ rule Sarek:
         reference = config['sarek']['reference'],
         targets = config['sarek']['targetregions'],
         intervals = config['sarek']['interval_padding'],
-        COSMIC = config['sarek']['COSMIC'],
         HMF_PON = config['sarek']['HMF_PON'],
         workdir = config['sarek']['workdir'],
-        outdir=lambda wildcards: f"{output_dir}sarek/{wildcards.sample}",
+        outdir=lambda wildcards: f"{output_dir}sarek/{wildcards.patient}",
     shell:
         """
-        nextflow -log {log} run nf-core/sarek -r 3.8.1 \
-            -profile {params.profile} \
-            -work-dir {params.workdir} \
-            --sample {wildcards.sample} \
-            --input {input} \
-            --outdir {params.outdir} \
-            --genome {params.genome} \
-            --tools {params.tools} \
-            --intervals {params.targets} \
-            --interval_padding {params.intervals} \
-            --max_cpus {threads} \
-            --fastp_max_cpus {threads} \
-            --bwa_max_cpus {threads} \
-            --gatk_max_cpus {threads} \
-            --max_memory '{resources.mem_mb} MB' \
-            --vep \
-            --vep_custom {params.COSMIC},COSMIC,vcf,exact,0,ID \
-            --vep_custom {params.HMF_PON},HMF_PON,vcf,exact,0 \
-            --save_mapped --save_output_as_bam \
-            --tumor_only \
-            --wes \
-            --mutect2_extra_args "--genotype-germline-sites true --genotype-pon-sites true"
-        -resume
-        
-        touch {output}
-        """
+        run_dir={params.workdir}/sessions/{wildcards.patient}
+        mkdir -p $run_dir
+        cd $run_dir
 
+        # Create samplesheet
+        awk -F',' '$1=="patient" || $1=="{wildcards.patient}"' {input} > {output.samplesheet}
+
+        nextflow -log {log} run nf-core/sarek -r 3.8.1 \
+           -profile {params.profile} \
+           -work-dir {params.workdir} \
+           -resume \
+              --input {output.samplesheet} \
+              --outdir {params.outdir} \
+              --genome {params.genome} \
+              --tools {params.tools} \
+              --intervals {params.targets} \
+              --interval_padding {params.intervals} \
+              --max_cpus {threads} \
+              --fastp_max_cpus {threads} \
+              --bwa_max_cpus {threads} \
+              --gatk_max_cpus {threads} \
+              --max_memory '{resources.mem_mb} MB' \
+              --vep \
+              --bcftools_annotations {params.HMF_PON} \
+              --save_mapped \
+              --wes \
+              --mutect2_extra_args "--genotype-germline-sites true --genotype-pon-sites true"
+        """
+#+++++++++++++++++++++++++++++++++++++++++ 2 PERFORM CNA ANALYSIS +++++++++++++++++++++++++++++++++++++++++++++
+# 2.1 Run CopywriteR, Normalize with QDNAseq and export results
+"""
+rule CNA_analysis:
+    input:
+        bam= output_dir + 'sarek/{patient}/preprocessing/recalibrated/{sample}/{sample}.recal.bam'
+    output:
+        output_dir + 'copywriter/{sample}/CNAprofiles/read_counts.txt'
+    params:
+genome = 'hg38',
+outdir = output_dir + 'copywriter/'
+cores = config'CopyWriteR']['cores']
+        binsize = config['CopyWriteR']['binsize'],
+
+    conda:
+        "envs/copywritr.yaml"
+    script:
+        'scripts/CNA_analysis.R'
+"""
---
+++ scripts/CNA_analysis.R
@@ -0,0 +1,161 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CNA_analysis.R
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# Run CopywriteR, Normalize with QDNAseq and export results
+#
+# Author: Jurriaan Janssen (j.janssen.1@erasmusmc.nl)
+#
+# condaenv: 
+# Usage: 
+#
+# TODO:
+# 1) 
+#
+# History:
+#  13-03-2026: File creation
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 0.1  Load packages
+#-------------------------------------------------------------------------------
+suppressMessages(library(dplyr))
+suppressMessages(library(CopywriteR))
+suppressMessages(library(QDNAseq))
+suppressMessages(library(Biobase))
+suppressMessages(library(GenomicRanges))
+
+#-------------------------------------------------------------------------------
+# 0.2 Parse command line arguments
+#-------------------------------------------------------------------------------
+if(exists("snakemake")){
+    input_bam <- snakemake@input[["bam"]]
+    sample <- snakemake@wildcards[["sample"]]
+    genome <- snakemake@params[["genome"]]
+    binsize <- snakemake@params[["binsize"]]
+    cores <- snakemake@params[["cores"]]
+    outdir <- snakemake@params[["outdir"]]
+}else{
+    input_bam <- 'output/sarek/MINT12/preprocessing/mapped/MINT12_tumor1/MINT12_tumor1.sorted.bam'
+    sample <- 'MINT12_tumor1'
+    genome <- 'hg38'
+    binsize <- '100000'
+    cores <- 10
+    outdir <- 'output/copywriter/'
+}
+
+#-------------------------------------------------------------------------------
+# 1.1 Define CopyWritR parameters
+#-------------------------------------------------------------------------------
+# Create annotation files
+if (!dir.exists(outdir)) dir.create(outdir, recursive = TRUE)
+preCopywriteR(output.folder = outdir,
+              bin.size = as.integer(binsize),
+              ref.genome = genome,
+              prefix = "chr")
+
+# get number of kb bins
+kbbin <- substring(binsize,1,nchar(binsize)-3)
+# Load annotation files
+load(file = file.path(outdir, paste0("hg38_",kbbin,"kb_chr"), "blacklist.rda"))
+# set number of workers
+bp.param <- SnowParam(workers = cores, type = "SOCK")
+# Create sample df
+sample.control <- data.frame(samples = input_bam,controls=input_bam)
+
+#-------------------------------------------------------------------------------
+# 2.1 Run CopyWritR
+#-------------------------------------------------------------------------------
+# Fetch and create sample output dir
+sample_dir <- paste0(outdir,sample,'/')
+if(!sample %in% list.files(paste0(getwd(),"/output/"))){
+    dir.create(sample_dir)
+}
+
+# Run CopyWriteR
+if(!"input.Rdata" %in% list.files(paste0(sample_dir,"/CNAprofiles/"))){
+    unlink(paste0(sample_dir,"/CNAprofiles/"), recursive = TRUE)
+    CopywriteR(sample.control = sample.control,
+               destination.folder = sample_dir,
+               reference.folder = file.path(outdir, paste0("hg38_",kbbin,"kb_chr")),
+               bp.param=bp.param)
+}
+
+
+#-------------------------------------------------------------------------------
+# 3.1 Parse Copywriter output
+#-------------------------------------------------------------------------------
+read_counts <- read.delim(paste0(sample_dir,'/CNAprofiles/read_counts.txt'))
+
+#-------------------------------------------------------------------------------
+# 2.1 Prepare QDNAseq objects
+#-------------------------------------------------------------------------------
+#---------- create bins file  ----------
+kbbin <- substring(binsize,1,nchar(binsize)-3)
+load(paste0(outdir,"hg38_",kbbin,"kb_chr/GC_mappability.rda"))
+
+# create dataframe containing fdata fields
+fData_all <-
+    cbind(as.data.frame(seqnames(GC.mappa.grange)),
+          as.data.frame(ranges(GC.mappa.grange)),
+          as.data.frame((mcols(GC.mappa.grange))))
+# create features as row names
+rownames(fData_all) <- paste0(fData_all$value,":",fData_all$start,"-",fData_all$end)
+
+bins <- getBinAnnotations(as.integer(kbbin))
+pData_bins <- pData(bins)
+features_bins <- paste0('chr',rownames(pData_bins))
+# remove weird features from bin data
+pData_bins <- pData_bins[!rownames(pData_bins) %in% features_bins[!features_bins %in% rownames(fData_all)], ]
+bins  <-pData_bins
+#--------------------------------------------
+#---------- create copynumber file ----------
+colnames(read_counts)[6] <- 'read_counts'
+read_counts$samples <- sample
+
+counts  <-
+    read_counts %>% 
+    dplyr::select(Feature,read_counts,samples) %>%
+    tidyr::spread(samples,read_counts,fill=0)
+
+counts <- counts[match(paste0('chr',rownames(bins)), counts$Feature),] %>%
+    dplyr::group_by(Feature) %>% dplyr::filter(dplyr::row_number() == 1 ) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na(Feature)) %>% 
+    tibble::column_to_rownames(.,"Feature") %>%
+    #tibble::add_column(bins = rownames(.), .before = colnames(.)[1]) %>%
+    as.matrix()
+
+#-------------------------------------------
+#---------- create phenodata file ----------
+phenodata <- data.frame(name = sample,
+           total.reads = colSums(counts),
+           used.reads = colSums(counts),
+           row.names = sample,
+           expected.variance = 0)
+
+#--------------------------------------------------
+#--------- create QDNAseqReadCounts object --------
+rownames(bins) <- paste0('chr',rownames(bins))
+features <- intersect(rownames(bins),rownames(counts))
+
+QDNAseqCopyNumbers <- new("QDNAseqReadCounts",bins=bins[features,],counts=as.matrix(counts[features,]),phenodata=phenodata)
+
+#-------------------------------------------------------------------------------
+# 2.1 Perform QDNAseq normalizations
+#-------------------------------------------------------------------------------
+
+ls()
+blacklist.grange
+GC.mappa.grange
+
+corrected <- applyFilters(QDNAseqCopyNumbers, residual=TRUE, blacklist=TRUE, mappability=FALSE, bases=FALSE , chromosomes=c('chrY','chrX','X','Y')) %>%
+    estimateCorrection() %>%
+    correctBins() %>%
+    normalizeBins() %>%
+    smoothOutlierBins() %>%
+    segmentBins() %>%
+    normalizeSegmentedBins()
+
+
+saveRDS('')
+
+exportBins(corrected,'test.bed', format = 'bed', type = 'segments')
